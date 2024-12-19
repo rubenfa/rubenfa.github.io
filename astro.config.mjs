a8fa4bdce57b36a2bf106158c9df1e4d8ce5997b
Rubén Fernández <ruben.fernandez@remote.com>
2024-12-19 12:04:15 +0100

    More blog page changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-i18n-starter.pages.dev',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
