098c4248b1282c317e277a318976490354ee8d73
Rubén Fernández <ruben.fernandez@remote.com>
2024-12-24 16:14:57 +0100

    use year folder

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,7 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
   type: 'content',
+  loader: glob({ pattern: "**/*.md", base: "./src/data/blog" })
   schema: ({ image }) =>
     z.object({
       title: z.string(),
